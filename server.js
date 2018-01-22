be214ad88a33958841b5aa0653d4ad09757cd831
Kristianto Lie <saibetik.kpn@gmail.com>
2018-01-23 01:53:06 +0800

    Moved server port to 8080.

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ app.get('/', (req, res) => {
   res.json(['Hello World!']);
 });
 
-app.set('SERVER_PORT', process.env.OPENSHIFT_NODEJS_PORT || 3000);
-app.set('SERVER_IP', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+// app.set('SERVER_PORT', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+// app.set('SERVER_IP', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-app.listen(app.get('SERVER_PORT'), app.get('SERVER_IP'), () => {
-  console.log('kopda-api listening on port ' + app.get('SERVER_PORT'));
+app.listen(8080, '0.0.0.0', () => {
+  console.log('kopda-api listening on port 8080');
 })
